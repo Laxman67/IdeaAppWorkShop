b6f8875c1ccd45fd7c29c20151301bd16311d890
laxman67 <laxmanhenricks94@gmail.com>
2023-07-05 03:03:40 +0530

    Added file and modified

--- models/user.module.js
+++
@@ -1,4 +0,0 @@
-/*
-This will hold the schema for user  */
-
-const mongoose = require("mongoose");
--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-const express = require("express");
-const serverconfig = require("./config/server-config");
+const express  = require('express');
+const serverConfig =require('./configs/server.config');
+const mongoose = require('mongoose');
+const dbConfig = require('./configs/db.config');
+
+
 
 const app = express();
 
-app.listen(serverconfig.PORT, () => {
-  console.log(`Server Started on the port number  ${serverconfig.PORT}`);
+
+/**
+ * Logic to connect to MongoDB and create an ADMIN user
+ * Need to have the mongodb up and running in your local machine
+ */
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection ;
+
+db.on("error", ()=>{
+    console.log("Error while connecting to DB");
 });
+
+db.once("open", ()=>{
+    console.log("DB is connected");
+
+    init();
+})
+
+app.listen(serverConfig.PORT, ()=>{
+  console.log(`server started on the port number ${serverConfig.PORT}` );
+})
