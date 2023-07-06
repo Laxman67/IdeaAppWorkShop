c9a3eac6de5457c63ad0cbcf954824b6601f7dd6
laxman67 <laxmanhenricks94@gmail.com>
2023-07-06 22:00:42 +0530

    adding user to db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const serverConfig = require("./configs/server.config");
 const mongoose = require("mongoose");
 const dbConfig = require("./configs/db.config");
+const userModel = require("./models/user.model");
 
 const app = express();
 
@@ -15,7 +16,32 @@ db.on("error", () => {
 
 db.once("open", () => {
   console.log(" Databse is  Connected");
+
+  init();
 });
+
+async function init() {
+  /*Initilze and create user
+   */
+
+  let admin = await userModel.findOne({
+    userId: "admin",
+  });
+
+  if (admin) {
+    console.log("Admin user already present");
+    return;
+  }
+
+  admin = await userModel.create({
+    name: "Laxman",
+    userId: "admin",
+    email: "laxmahenricks94@gmail.com",
+    userType: "ADMIN",
+    password: "laxman@781",
+  });
+  console.log(admin);
+}
 
 app.listen(serverConfig.PORT, () => {
   console.log(`server started on the port number ${serverConfig.PORT}`);
